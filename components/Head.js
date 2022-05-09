c955c90668d46bf883c30cb1aea778f3be96231c
jeececab <jcseguincabana@gmail.com>
2022-05-09 07:49:11 -0400

    Fix unknown prop

--- components/Head.js
+++ components/Head.js
@@ -6,7 +6,7 @@ export default function Head({ title, description }) {
       <meta charset="utf-8"></meta>
       {/* <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta> */}
       <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"></meta>
-      <meta http-equiv="x-ua-compatible" content="ie=edge"></meta>
+      <meta httpEquiv="x-ua-compatible" content="ie=edge"></meta>
       <link rel="icon" href="/favicon.ico" />
       <title>{title}</title>
       <meta name="description" content={description} />
