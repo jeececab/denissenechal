1f4aeb572a2c29729097e9af2e60daadf1b508a9
jeececab <jcseguincabana@gmail.com>
2022-05-10 18:08:17 -0400

    carousel mobile fix

--- components/Home/HomeCarousel.js
+++ components/Home/HomeCarousel.js
@@ -1,14 +1,17 @@
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import styles from '../../styles/HomeCarousel.module.css';
 
 export default function HomeCarousel() {
   return (
     <div className="max-width-container">
       <Carousel showThumbs={false} autoPlay infiniteLoop interval={5000}>
-        <div>
+        <div className={styles.image}>
+          <img src="/assets/images/golf-mobile.png" />
           <img src="/assets/images/golf.png" />
         </div>
-        <div>
+        <div className={styles.image}>
+          <img src="/assets/images/serveur-mobile.png" />
           <img src="/assets/images/serveur.png" />
         </div>
       </Carousel>
